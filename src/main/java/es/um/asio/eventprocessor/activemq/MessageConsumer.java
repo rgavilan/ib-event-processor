cb1bfc0c5bd73bff9a2987e8ee5fa309861cb1a2
Alejandro Martino Uria <amartino@izertis.com>
2020-11-18 11:16:12 +0100

    SEMANTMURC-381 activeMQ

--- src/main/java/es/um/asio/eventprocessor/activemq/MessageConsumer.java
+++ src/main/java/es/um/asio/eventprocessor/activemq/MessageConsumer.java
@@ -2,10 +2,12 @@ package es.um.asio.eventprocessor.activemq;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
 import es.um.asio.abstractions.domain.ManagementBusEvent;
+import es.um.asio.eventprocessor.service.MessageService;
 
 /**
  * The Class MessageConsumer.
@@ -14,6 +16,9 @@ import es.um.asio.abstractions.domain.ManagementBusEvent;
 public class MessageConsumer {
 
 	private final Logger logger = LoggerFactory.getLogger(MessageConsumer.class);
+	
+	  @Autowired
+	    private MessageService messageService;
 
     /**
      * Listener.
@@ -22,6 +27,14 @@ public class MessageConsumer {
      */
     @JmsListener(destination = "${app.activemq.queue-name:default-queue-name}")
     public void listener(final ManagementBusEvent message){
-        logger.info("Message received {} ", message);
+        
+    	// remove it. It's only for debugging
+    	logger.info("Message received {} ", message);
+        
+        if (this.logger.isDebugEnabled()) {
+            this.logger.debug("Received message: {}", message);
+        }
+
+        this.messageService.process(message);
     }
 }
